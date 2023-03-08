75b56fb43e33916a5809fe539d48575d907f81ac
Milos Petrasinovic <milos.petrasinovic@sap.com>
2023-03-08 12:50:02 +0100

    Add circuitbreaker

---
+++ src/main/java/dev/mpetrasin/edgeservice/web/WebEndpoints.java
@@ -0,0 +1,22 @@
+package dev.mpetrasin.edgeservice.web;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.RouterFunctions;
+import org.springframework.web.reactive.function.server.ServerResponse;
+import reactor.core.publisher.Mono;
+
+public class WebEndpoints {
+
+    @Bean
+    public RouterFunction<ServerResponse> routerFunction() {
+        return RouterFunctions.route()
+                .GET("/catalog-fallback", request -> ServerResponse.ok()
+                        .body(Mono.just(""), String.class))
+                .POST("/catalog-fallback", request -> ServerResponse.status(HttpStatus.SERVICE_UNAVAILABLE).build())
+                .build();
+    }
+
+
+}
