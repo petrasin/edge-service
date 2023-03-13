74ae9e5210406cdecbbd274fcd87027c42c90567
mpetrasin <milos.petrasin@gmail.com>
2023-03-13 10:02:40 +0100

    Log registered routes

--- src/main/java/dev/mpetrasin/edgeservice/EdgeServiceApplication.java
+++ src/main/java/dev/mpetrasin/edgeservice/EdgeServiceApplication.java
@@ -1,15 +1,37 @@
 package dev.mpetrasin.edgeservice;
 
+import jakarta.annotation.PostConstruct;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.gateway.route.RouteLocator;
-import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class EdgeServiceApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(EdgeServiceApplication.class, args);
-    }
-    }
+  private static final Logger logger = LoggerFactory.getLogger(EdgeServiceApplication.class);
+
+  private final RouteLocator routeLocator;
+
+  public EdgeServiceApplication(RouteLocator routeLocator) {
+    this.routeLocator = routeLocator;
+  }
+
+  public static void main(String[] args) {
+    SpringApplication.run(EdgeServiceApplication.class, args);
+  }
+
+
+  @PostConstruct
+  public void printRouteInfo() {
+    routeLocator.getRoutes().subscribe(route -> {
+      var id = route.getId();
+      var predicate = route.getPredicate().toString();
+      var uri = route.getUri().toString();
+
+      logger.info("Registered route{id: {}, predicate: {}, target: {}}", id, predicate, uri);
+    });
+  }
+}
