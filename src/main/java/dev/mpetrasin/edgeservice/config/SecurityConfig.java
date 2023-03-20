77413fc1d91d6d0792e216e0e8401579f2019a73
mpetrasin <milos.petrasin@gmail.com>
2023-03-20 22:17:55 +0100

    Configuring RP-Initiated Logout

--- src/main/java/dev/mpetrasin/edgeservice/config/SecurityConfig.java
+++ src/main/java/dev/mpetrasin/edgeservice/config/SecurityConfig.java
@@ -5,18 +5,35 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.oauth2.client.oidc.web.server.logout.OidcClientInitiatedServerLogoutSuccessHandler;
+import org.springframework.security.oauth2.client.registration.ReactiveClientRegistrationRepository;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.web.server.authentication.logout.ServerLogoutSuccessHandler;
 
 @Configuration
 @EnableWebFluxSecurity
 public class SecurityConfig {
 
-    @Bean
-    SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
-        return http
-                .authorizeExchange(exchange -> exchange.anyExchange().authenticated())
-                .oauth2Login(Customizer.withDefaults())
-                .build();
-    }
+  @Bean
+  SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http,
+                                                   ReactiveClientRegistrationRepository clientRegistrationRepository) {
+    return http
+        .authorizeExchange(exchange -> exchange.anyExchange().authenticated())
+        .oauth2Login(Customizer.withDefaults())
+        .logout(logout -> logout.logoutSuccessHandler(oidcLogoutSuccessHandler(
+            clientRegistrationRepository
+        )))
+        .build();
+  }
+
+  private ServerLogoutSuccessHandler oidcLogoutSuccessHandler(
+      ReactiveClientRegistrationRepository clientRegistrationRepository
+  ) {
+    var oidLogoutSuccessHandler =
+        new OidcClientInitiatedServerLogoutSuccessHandler(clientRegistrationRepository);
+    oidLogoutSuccessHandler.setPostLogoutRedirectUri("{baseUrl}");
+    return oidLogoutSuccessHandler;
+  }
+
 
 }
