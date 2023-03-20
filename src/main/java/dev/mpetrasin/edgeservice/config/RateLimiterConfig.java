1d88bf35eef8a6762f1eaa8924a0cc3bce5ce7b5
mpetrasin <milos.petrasin@gmail.com>
2023-03-20 21:40:23 +0100

    Authenticate users with OpenId Connect

--- src/main/java/dev/mpetrasin/edgeservice/config/RateLimiterConfig.java
+++ src/main/java/dev/mpetrasin/edgeservice/config/RateLimiterConfig.java
@@ -3,16 +3,16 @@ package dev.mpetrasin.edgeservice.config;
 import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import reactor.core.publisher.Mono;
+
+import java.security.Principal;
 
 @Configuration
 public class RateLimiterConfig {
 
-
-    // Until spring security is configured, use the same bucket
-    // for each request.
-    @Bean
-    public KeyResolver keyResolver() {
-        return exchange -> Mono.just("anonymous");
-    }
+  @Bean
+  public KeyResolver keyResolver() {
+    return exchange -> exchange.getPrincipal()
+             .map(Principal::getName)
+             .defaultIfEmpty("anonymous");
+  }
 }
