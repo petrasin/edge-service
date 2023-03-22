853831975b6c16937dbab4fffa78d76069d558de
Milos Petrasinovic <milos.petrasinovic@sap.com>
2023-03-22 20:33:58 +0100

    Add Single-Page UI route

--- src/main/java/dev/mpetrasin/edgeservice/EdgeServiceApplication.java
+++ src/main/java/dev/mpetrasin/edgeservice/EdgeServiceApplication.java
@@ -1,15 +1,10 @@
 package dev.mpetrasin.edgeservice;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class EdgeServiceApplication {
-
-  private static final Logger logger =
-      LoggerFactory.getLogger(EdgeServiceApplication.class);
 
   public static void main(String[] args) {
     SpringApplication.run(EdgeServiceApplication.class, args);
