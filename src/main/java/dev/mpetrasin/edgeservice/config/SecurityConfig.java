63158a16215a3ea60873096649d6904b546e4e38
mpetrasin <milos.petrasin@gmail.com>
2023-03-22 21:09:25 +0100

    Control auth flow from SPA

--- src/main/java/dev/mpetrasin/edgeservice/config/SecurityConfig.java
+++ src/main/java/dev/mpetrasin/edgeservice/config/SecurityConfig.java
@@ -2,12 +2,15 @@ package dev.mpetrasin.edgeservice.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.oauth2.client.oidc.web.server.logout.OidcClientInitiatedServerLogoutSuccessHandler;
 import org.springframework.security.oauth2.client.registration.ReactiveClientRegistrationRepository;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.web.server.authentication.HttpStatusServerEntryPoint;
 import org.springframework.security.web.server.authentication.logout.ServerLogoutSuccessHandler;
 
 @Configuration
@@ -18,7 +21,17 @@ public class SecurityConfig {
   SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http,
                                                    ReactiveClientRegistrationRepository clientRegistrationRepository) {
     return http
-        .authorizeExchange(exchange -> exchange.anyExchange().authenticated())
+        .authorizeExchange(exchange -> exchange
+            .pathMatchers("/", "/*.css", "/*.js", "/favicon.ico")
+              .permitAll()
+            .pathMatchers(HttpMethod.GET,"/books/**")
+              .permitAll()
+            .anyExchange().authenticated())
+        .exceptionHandling(exceptionHandlingSpec ->
+            exceptionHandlingSpec.authenticationEntryPoint(
+                new HttpStatusServerEntryPoint(HttpStatus.UNAUTHORIZED)
+            )
+        )
         .oauth2Login(Customizer.withDefaults())
         .logout(logout -> logout.logoutSuccessHandler(oidcLogoutSuccessHandler(
             clientRegistrationRepository
