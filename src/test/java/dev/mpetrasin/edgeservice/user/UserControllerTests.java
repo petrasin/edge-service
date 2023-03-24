54889cb4f1b79334cbf2067c9a5b0c47ec742081
mpetrasin <milos.petrasin@gmail.com>
2023-03-24 23:09:42 +0100

    Test OIDC authentication

---
+++ src/test/java/dev/mpetrasin/edgeservice/user/UserControllerTests.java
@@ -0,0 +1,62 @@
+package dev.mpetrasin.edgeservice.user;
+
+import dev.mpetrasin.edgeservice.config.SecurityConfig;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
+import org.springframework.security.oauth2.client.registration.ReactiveClientRegistrationRepository;
+import org.springframework.security.oauth2.core.oidc.StandardClaimNames;
+import org.springframework.security.test.web.reactive.server.SecurityMockServerConfigurers;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+import java.util.List;
+
+@WebFluxTest(UserController.class)
+@Import(SecurityConfig.class)
+class UserControllerTests {
+
+  @Autowired
+  WebTestClient webClient;
+
+  @MockBean
+  ReactiveClientRegistrationRepository clientRegistrationRepository;
+
+  @Test
+  void whenNotAuthenticatedThen401() {
+    webClient.get()
+             .uri("/user")
+             .exchange()
+             .expectStatus().isUnauthorized();
+
+  }
+
+  @Test
+  void whenAuthenticatedThenReturnUser() {
+
+    var expectedUser = new User("john.snow", "John", "Snow",
+        List.of("employee", "customer"));
+
+    webClient
+        .mutateWith(configureMockOidcLogin(expectedUser))
+        .get()
+        .uri("/user")
+        .exchange()
+        .expectStatus().is2xxSuccessful()
+        .expectBody(User.class)
+        .isEqualTo(expectedUser);
+  }
+
+  private SecurityMockServerConfigurers.OidcLoginMutator configureMockOidcLogin(User expectedUser) {
+
+    return SecurityMockServerConfigurers.mockOidcLogin().idToken(
+        builder -> builder
+            .claim(StandardClaimNames.PREFERRED_USERNAME,
+                expectedUser.username())
+            .claim(StandardClaimNames.GIVEN_NAME, expectedUser.firstName())
+            .claim(StandardClaimNames.FAMILY_NAME, expectedUser.lastName())
+    );
+  }
+
+}
