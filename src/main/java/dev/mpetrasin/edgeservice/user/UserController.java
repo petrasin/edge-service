7c6236763429fea8f15e2e4f335da25bec1d7843
mpetrasin <milos.petrasin@gmail.com>
2023-05-02 10:25:41 +0200

    Add logger

--- src/main/java/dev/mpetrasin/edgeservice/user/UserController.java
+++ src/main/java/dev/mpetrasin/edgeservice/user/UserController.java
@@ -5,13 +5,17 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import lombok.extern.slf4j.Slf4j;
 import reactor.core.publisher.Mono;
 
+@Slf4j
 @RestController
 public class UserController {
 
   @GetMapping("user")
   public Mono<User> getUser(@AuthenticationPrincipal OidcUser oidcUser) {
+    log.info("Fetching information about the currently authenticated user");
     var user = new User(
         oidcUser.getPreferredUsername(),
         oidcUser.getGivenName(),
